3b181e1242029dc4c7ba003d357053b7a44075cc
FIGUEIRAS Jossua <105790280+Mandicore@users.noreply.github.com>
2023-07-25 23:09:08 +0200

    avancements problème scroll

--- money management/money_management.py
+++ money management/money_management.py
@@ -29,7 +29,7 @@ def AppWindow(users):
     app.title("Money Management")
     app.geometry(f"{windowWidth}x{windowHeight}+{x_position}+{y_position}")
     app.configure(bg="#a4adf9")
-    background_plate = Canvas(app, bg="lightgrey", width=((windowWidth/10)*4), height=((windowHeight/10)*8))
+    background_plate = Canvas(app, bg="lightgrey", width=((windowWidth/10)*2), height=((windowHeight/10)*8))
     background_plate.place(relx=0.5, rely=0.5, anchor=CENTER)
     ContainSelectProfil(users, background_plate, windowWidth, windowHeight)
 
@@ -53,26 +53,21 @@ def ContainSelectProfil(users, background_plate, windowWidth, windowHeight):
     #button_frame = Frame(canvas, bg="lightgrey")
     #canvas.create_window((((windowWidth/10)*4), ((windowHeight/10)*8)), window=button_frame, anchor="nw")
 
+    title_label = Label(background_plate, text="Choisissez votre profil", font=("Arial", 16), padx=15, pady=10 )
+    title_label.pack()
+
     for user in users:
-        button = Button(background_plate, text=user,
-                           bg="blue",       # Couleur de fond du bouton (ici bleu)
-                           fg="white",      # Couleur du texte du bouton (ici blanc)
-                           font=("Arial", 12),  # Police et taille du texte
-                           padx=10,         # Espace horizontal (marge intérieure) du bouton
-                           pady=5           # Espace vertical (marge intérieure) du bouton
-                           )
-        button.pack(fill="x", pady=2)
+        button = Button(background_plate, text=user, bg="blue", fg="white", font=("Arial", 12), padx=10, pady=5)
+        button.pack()
+
+    # Définir la région de défilement du Canvas
+    background_plate.configure(scrollregion=background_plate.bbox("all"))
+    background_plate.pack()
 
     # Définir une barre de défilement
-    scrollbar = Scrollbar(app, command=background_plate.yview)
+    scrollbar = Scrollbar(background_plate, command=background_plate.yview)
     background_plate.configure(yscrollcommand=scrollbar.set)
     scrollbar.pack(side="right", fill="y")
-
-    # Configurer le Canvas pour gérer le défilement
-    background_plate.bind_all("<MouseWheel>", lambda event: background_plate.yview_scroll(-1 * int(event.delta / 120), "units"))
-
-    # Configurer la taille du Canvas pour qu'il s'ajuste en fonction du contenu
-    background_plate.bind("<Configure>", lambda event: background_plate.configure(scrollregion=background_plate.bbox("all")))
 
 
 
@@ -83,7 +78,7 @@ def ContainSelectProfil(users, background_plate, windowWidth, windowHeight):
 
 def usersInformations():
     """Return all users informations ..."""
-    return ["Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5, Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 111", "Utilisateur 222","Utilisateur 333","Utilisateur 444","Utilisateur 555"]
+    return ["Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5555", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 666"]
 
 
 
