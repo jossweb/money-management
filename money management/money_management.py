79565364ceb51487b96f4f50fc57b9407789b0b9
FIGUEIRAS Jossua <105790280+Mandicore@users.noreply.github.com>
2023-07-27 11:19:50 +0200

    création de la selection de profil avec scroll terminé !

--- money management/money_management.py
+++ money management/money_management.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import width
 
 
 def SetWindowSettings(screenWidth, screenHeight, setting):
@@ -31,54 +32,24 @@ def AppWindow(users):
     app.configure(bg="#a4adf9")
     background_plate = Canvas(app, bg="lightgrey", width=((windowWidth/10)*2), height=((windowHeight/10)*8))
     background_plate.place(relx=0.5, rely=0.5, anchor=CENTER)
-    ContainSelectProfil(users, background_plate, windowWidth, windowHeight)
+    #ContainSelectProfil(users, background_plate, windowWidth, windowHeight)
+    testscroll(background_plate, users)
 
-
-def ContainSelectProfil(users, background_plate, windowWidth, windowHeight):
-    """print on screen one button by users detect"""
-    ##locationY = 60;
-    ##for i in range(len(users)):
-     ##button = Button(background_plate, text="Utilisateur : " + users[i], command=on_button_click, bg="blue", fg="white", font=("Arial", 12), padx=10, pady=5)
-     ##button.pack(pady=locationY)
-     ##locationY = 20
-
-
-
-     ######Test----------------------------------------------------------------------------
-
-    #canvas = Canvas(app, bg="lightgrey", width=((windowWidth/10)*4), height=((windowHeight/10)*8), highlightthickness=0)
-    #canvas.pack(fill="both")
-
-    # Créer un Frame pour contenir les boutons
-    #button_frame = Frame(canvas, bg="lightgrey")
-    #canvas.create_window((((windowWidth/10)*4), ((windowHeight/10)*8)), window=button_frame, anchor="nw")
-
-    title_label = Label(background_plate, text="Choisissez votre profil", font=("Arial", 16), padx=15, pady=10 )
-    title_label.pack()
-
+def testscroll(app, users):
+    scrollbar = Scrollbar(app)
+    scrollbar.pack( side = RIGHT, fill = Y)
+    liste = Listbox(app, yscrollcommand = scrollbar.set, font=("Arial", 15), justify="center", selectbackground="lightgrey", selectforeground="black", activestyle="none")
     for user in users:
-        button = Button(background_plate, text=user, bg="blue", fg="white", font=("Arial", 12), padx=10, pady=5)
-        button.pack()
-
-    # Définir la région de défilement du Canvas
-    background_plate.configure(scrollregion=background_plate.bbox("all"))
-    background_plate.pack()
-
-    # Définir une barre de défilement
-    scrollbar = Scrollbar(background_plate, command=background_plate.yview)
-    background_plate.configure(yscrollcommand=scrollbar.set)
-    scrollbar.pack(side="right", fill="y")
+       liste.insert(END, user)
+       liste.itemconfigure(END, bg="white", )
 
 
-
-
-
-    #FIN TEST ---------------------------------------------------------------------------------------------------------------------------------------
-
+    liste.pack(side = LEFT, fill = BOTH )
+    scrollbar.config(command = liste.yview )
 
 def usersInformations():
     """Return all users informations ..."""
-    return ["Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5555", "Utilisateur 1", "Utilisateur 2", "Utilisateur 3", "Utilisateur 4", "Utilisateur 666"]
+    return ["FIGUEIRAS Jossua", "vingt caractere max1", "Utilisateur 3", "Utilisateur 4", "Utilisateur 5"]
 
 
 
