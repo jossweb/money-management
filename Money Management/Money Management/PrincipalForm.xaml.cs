c54c1cff2636d495668e457b430102a5ec18ff84
Jossua Figueiras <jossufigueiras@icloud.com>
2023-10-03 22:08:05 +0200

    ajout de nouvelle fonctionnalité au graphique de test et code commenté pour mieux comprendre. Passage à la maquette papier

--- Money Management/Money Management/PrincipalForm.xaml.cs
+++ Money Management/Money Management/PrincipalForm.xaml.cs
@@ -3,6 +3,8 @@ using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Money_Management
 {
@@ -20,27 +22,40 @@ namespace Money_Management
             // Création du graphique
             var graphique = new CartesianChart();
 
-            var axeY = new AxesCollection();
-            axeY.Add(new Axis
+            var axeX = new AxesCollection();
+            axeX.Add(new Axis
             {
                 Title = "Étiquettes",
                 Labels = étiquettes
             });
 
-            graphique.AxisY = axeY;
+            graphique.AxisX = axeX;
 
             graphique.Series = new SeriesCollection
             {
-                new RowSeries
+                new ColumnSeries // Utilisation de ColumnSeries pour un graphique à barres verticales
                 {
                     Title = "Valeurs",
                     Values = valeurs,
-                    DataLabels = true
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Colors.Red) // Changement de la couleur des barres
                 }
             };
 
-            // Ajout du graphique à la fenêtre
-            Content = graphique;
+            // Création d'une grille pour centrer le graphique
+            var grille = new Grid();
+            grille.HorizontalAlignment = HorizontalAlignment.Center;
+            grille.VerticalAlignment = VerticalAlignment.Center;
+
+            // Ajout du graphique à la grille
+            grille.Children.Add(graphique);
+
+            // Ajout de la grille à la fenêtre
+            Content = grille;
+
+            // Définir la taille du graphique
+            graphique.Width = 400; // Ajustez la taille selon vos besoins
+            graphique.Height = 300; // Ajustez la taille selon vos besoins
         }
     }
 }
