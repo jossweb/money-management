4fa29987c5f6c987e9f54441bd06fdb5649fa050
Jossua Figueiras <jossufigueiras@icloud.com>
2023-11-25 23:19:34 +0100

    Ajout de conditions d'inscriptions

--- Money Management/Money Management/SignUp.xaml.cs
+++ Money Management/Money Management/SignUp.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,8 +25,8 @@ namespace Money_Management
         private MySqlConnection connection;
         public string name { get; set; }
         public string firstName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
         public string PasswordConfirmed { get; set; }
         public DateTime BirstdayDate { get; set; }
         public SignUp(MySqlConnection connection)
@@ -46,14 +47,61 @@ namespace Money_Management
         }
         public void ConnectionButtonClick()
         {
-            if ((name != null) && (firstName != null) && (Email != null) && (Password != null) && (PasswordConfirmed != null))
+            if ((name != null) && (firstName != null) && (email != null) && (password != null) && (PasswordConfirmed != null))
             {
-                
+                if ((name.Length > 50) && (firstName.Length > 50) && (email.Length > 50) && (password.Length > 50) && (PasswordConfirmed.Length > 50))
+                {
+                    if ((firstName.Length < 3) && (name.Length < 3) && (email.Length < 3))
+                    {
+                        if (email.Contains("@"))
+                        {
+                            if (password == PasswordConfirmed)
+                            {
+                                if (password.Length > 8)
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    ErrorWindow error = new ErrorWindow("ERREUR: Votre mot de passe doit contenir au moins 8 caractères");
+                                    error.Show();
+                                    Debug.WriteLine("ERROR : The password contains less than 8 characters");
+                                }
+                            }
+                            else
+                            {
+                                ErrorWindow error = new ErrorWindow("ERREUR: Vos mots de passes ne correspondent pas !");
+                                error.Show();
+                                Debug.WriteLine("ERROR : Two passwords do not match");
+                            }
+                        }
+                        else
+                        {
+                            ErrorWindow error = new ErrorWindow("ERREUR: Votre adresse email doit contenir un caractère '@' !");
+                            error.Show();
+                            Debug.WriteLine("ERROR : email does not contain '@'");
+                        }
+                    }
+                    else
+                    {
+                        ErrorWindow error = new ErrorWindow("ERREUR: Les champs 'nom', 'prénom' et 'email' doivent contenir au minimum 3 caractères !");
+                        error.Show();
+                        Debug.WriteLine("ERROR : Last name, first name or email fields contain less than 3 characters");
+                    }
+
+                }
+                else
+                {
+                    ErrorWindow error = new ErrorWindow("ERREUR: Les champs doivent chacun contenir moins de 50 caractères !");
+                    error.Show();
+                    Debug.WriteLine("ERROR: One or more fields contain more than 50 characters!");
+                }     
             }
             else
             {
                 ErrorWindow error = new ErrorWindow("ERREUR: Veuillez remplir tous les champs du formulaire !");
                 error.Show();
+                Debug.WriteLine("ERROR: Not all fields are completed!!");
             }
         }
     }
--- Money Management/Money Management/obj/Debug/net7.0-windows/Money Management.AssemblyInfo.cs
+++ Money Management/Money Management/obj/Debug/net7.0-windows/Money Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Money Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e6ef383eb5fbe386b76c3ab37bb2f872efb8b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Money Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Money Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
